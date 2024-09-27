be001f95bc1cc16d51e92536d338399509fc9ec1
aminmirzaei <aminmirzaei.og@gmail.com>
2024-09-27 13:08:25 +0330

    - Create Abogadro Constant

--- MathLiteVb/Constants.vb
+++ MathLiteVb/Constants.vb
@@ -1,5 +1,6 @@
 ﻿Structure Constants
     Public Const PI As Double = 3.1415926535897931
     Public Const Euler As Double = 2.7182818284590451
+    Public Const Avogadro As Double = 6.02214076E+23
 
 End Structure
