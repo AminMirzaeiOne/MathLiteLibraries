a71b73b6403c5803a12371eaf150ba7dac807b1d
aminmirzaei <aminmirzaei.og@gmail.com>
2024-09-27 13:10:25 +0330

    - Create Aperys Constant

--- MathLiteVb/Constants.vb
+++ MathLiteVb/Constants.vb
@@ -6,5 +6,6 @@
     Public Const SpeedOfLight As Double = 299792458
     Public Const Gravitational As Double = 0.000000000066743
     Public Const Gas As Double = 8.314462618
+    Public Const Aperys As Double = 1.202056903159594
 
 End Structure
