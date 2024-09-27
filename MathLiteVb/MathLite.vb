64fda9beb6960cd035158bd91d2520363b691457
aminmirzaei <aminmirzaei.og@gmail.com>
2024-09-27 07:38:32 +0330

    - Create Maximum Function In MathLite

--- MathLiteVb/MathLite.vb
+++ MathLiteVb/MathLite.vb
@@ -13,6 +13,23 @@
     Private _tanhValues() As Double
     Private _step As Double
 
+    Public Function Maximum(ParamArray numbers As Integer()) As Integer
+        If numbers Is Nothing OrElse numbers.Length = 0 Then
+            Throw New System.ArgumentException("Array cannot be null or empty.")
+        End If
+
+        Dim max As Integer = numbers(0)
+        For i As Integer = 1 To numbers.Length - 1
+            If numbers(i) > max Then
+                max = numbers(i)
+            End If
+        Next
+
+        Return max
+    End Function
+
+
+
 
 
 
