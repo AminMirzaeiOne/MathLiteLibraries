645f3ff1b15a193e6bb4bd31f39a1e59083b8c0f
aminmirzaei <aminmirzaei.og@gmail.com>
2024-09-27 04:16:52 +0330

    - Rename Math To MathLite Class - Create Max Function In MathLite Class

---
+++ MathLiteCs/MathLite.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLiteCs
+{
+    public static class MathLite
+    {
+        public static int Max(params int[] numbers)
+        {
+            if (numbers == null || numbers.Length == 0)
+            {
+                throw new ArgumentException("Array cannot be null or empty.");
+            }
+
+            int max = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] > max)
+                {
+                    max = numbers[i];
+                }
+            }
+
+            return max;
+        }
+    }
+}
