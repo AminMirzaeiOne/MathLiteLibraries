a4132be682343b09c5ca5881651095d4ee553167
aminmirzaei <aminmirzaei.og@gmail.com>
2024-09-27 05:52:03 +0330

    - Create Planck Constants

--- MathLiteCs/Constants.cs
+++ MathLiteCs/Constants.cs
@@ -11,5 +11,6 @@ namespace MathLiteCs
         public const double PI = 3.1415926535897931;
         public const double Euler = 2.718281828459045;
         public const double Avogadro = 6.02214076e23;
+        public const double Planck = 6.62607015e-34;
     }
 }
