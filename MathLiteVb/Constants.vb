86ac13ebe7ece4656307180d939841b1f77c9fd9
aminmirzaei <aminmirzaei.og@gmail.com>
2024-09-27 13:10:01 +0330

    - Create Gas Constant

--- MathLiteVb/Constants.vb
+++ MathLiteVb/Constants.vb
@@ -5,5 +5,6 @@
     Public Const Planck As Double = 6.62607015E-34
     Public Const SpeedOfLight As Double = 299792458
     Public Const Gravitational As Double = 0.000000000066743
+    Public Const Gas As Double = 8.314462618
 
 End Structure
