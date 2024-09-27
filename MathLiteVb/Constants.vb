98ddc6b25190c9bb7bc87befdae30d479ba8a8ee
aminmirzaei <aminmirzaei.og@gmail.com>
2024-09-27 13:08:48 +0330

    - Create Planck Constant

--- MathLiteVb/Constants.vb
+++ MathLiteVb/Constants.vb
@@ -2,5 +2,6 @@
     Public Const PI As Double = 3.1415926535897931
     Public Const Euler As Double = 2.7182818284590451
     Public Const Avogadro As Double = 6.02214076E+23
+    Public Const Planck As Double = 6.62607015E-34
 
 End Structure
