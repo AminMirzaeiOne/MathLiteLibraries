1c5dfd006ffe3b162c2dfd7cd38988b27803fd52
aminmirzaei <aminmirzaei.og@gmail.com>
2024-09-27 13:10:47 +0330

    - Create GoldenRatio Constant

--- MathLiteVb/Constants.vb
+++ MathLiteVb/Constants.vb
@@ -7,5 +7,6 @@
     Public Const Gravitational As Double = 0.000000000066743
     Public Const Gas As Double = 8.314462618
     Public Const Aperys As Double = 1.202056903159594
+    Public Const GoldenRatio As Double = 1.6180339887
 
 End Structure
