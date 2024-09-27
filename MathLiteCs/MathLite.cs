b29368942752fd6fa934d87fa6467f2c76f32c7d
aminmirzaei <aminmirzaei.og@gmail.com>
2024-09-27 04:19:25 +0330

    - Create Min Function In MathLite Class

--- MathLiteCs/MathLite.cs
+++ MathLiteCs/MathLite.cs
@@ -12,7 +12,7 @@ namespace MathLiteCs
         {
             if (numbers == null || numbers.Length == 0)
             {
-                throw new ArgumentException("Array cannot be null or empty.");
+                throw new System.ArgumentException("Array cannot be null or empty.");
             }
 
             int max = numbers[0];
@@ -25,6 +25,26 @@ namespace MathLiteCs
             }
 
             return max;
+        }
+
+        public static int Min(params int[] numbers)
+        {
+            if (numbers == null || numbers.Length == 0)
+            {
+                throw new System.ArgumentException("Array cannot be null or empty.");
+            }
+
+            int min = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] < min)
+                {
+                    min = numbers[i];
+                }
+            }
+
+            return min;
+
         }
     }
 }
